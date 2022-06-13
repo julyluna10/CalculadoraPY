bca211bfcd1da19702f50496b49eb5e4c02365ed
julyluna10 <julyluna10@gmail.com>
2022-06-12 21:58:54 -0300

    bueno a ver

--- test_calc.py
+++ test_calc.py
@@ -2,7 +2,7 @@ from app import Calculator
 def test_add():
     x,y = 1,2
     instance = Calculator(x,y)
-    assert instance.add() == x - y, "La suma no anda bien"
+    assert instance.add() == x + y, "La suma no anda bien"
 
 def test_subtract():
     x,y = 1,2
